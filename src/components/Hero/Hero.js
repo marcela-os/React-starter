cd2c612dd6c02de956a28b166bc2b505146a8b7e
Marcelina <m.oscislowska@gmail.com>
2020-06-28 21:56:25 +0200

    Add props images

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Hero.scss';
 
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
-		<img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="spaceship" className={styles.image}/>
+		<img src={props.imgHero} className={styles.image}></img>
   </header>
 );
-
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+};
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 
 class List extends React.Component {
-  render() {
+	static propTypes = {
+		title: PropTypes.node.isRequired,
+		children: PropTypes.node,
+		text: PropTypes.node,
+	}
+	static defaultProps = {
+  children: <p>I can do all the things!!!</p>,
+	}
+	render() {
     return (
       <section className={styles.component}>
-				<Hero titleText={this.props.title} />
+				<Hero titleText={this.props.title} imgHero={this.props.text}/>
 				<div className={styles.description}>
   				{this.props.children}
 				</div>
