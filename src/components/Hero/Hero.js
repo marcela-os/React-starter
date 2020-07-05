394d096dd4d7e8d02dd652e32b950fb293b43459
Marcelina <m.oscislowska@gmail.com>
2020-07-05 09:33:17 +0200

    Add addColumn state

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Hero.scss';
+import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>{props.titleText}</h2>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
 		<img src={props.imgHero} className={styles.image}></img>
   </header>
 );
