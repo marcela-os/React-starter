73c116d81e2982385bb7d03c9cad53d31d5dadfd
Marcelina <m.oscislowska@gmail.com>
2020-07-19 14:06:44 +0200

    Add ADD_CARD

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
-//import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
 import Icon from './Icon.js';
 
 class Column extends React.Component {
@@ -11,9 +11,15 @@ class Column extends React.Component {
 		title: PropTypes.node,
 		cards: PropTypes.array,
 		icon: PropTypes.node,
+		addCard: PropTypes.func,
 	}
+
+	static defaultProps = {
+		icon: settings.defaultColumnIcon,
+	}
+
 	render() {
-		const {title, icon, cards} = this.props;
+		const {title, icon, cards, addCard} = this.props;
 		return (
 			<section className={styles.component}>
 				<h3 className={styles.title}>{title} <span className={styles.icon}><Icon name={icon}/></span></h3>
@@ -22,14 +28,9 @@ class Column extends React.Component {
 						<Card key={cardData.id} {...cardData} />
 					))}
 				</div>
-				{/*<div className={styles.cards}>
-					{this.state.cards.map(({key, ...cardProps}) => (
-						<Card key={key} {...cardProps} />
-					))}
-				</div>
 				<div className={styles.creator}>
-					<Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-				</div>*/}
+					<Creator text={settings.cardCreatorText} action={addCard}/>
+				</div>
 			</section>
 		);
 	}
--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,10 +1,18 @@
 import {connect} from 'react-redux';
 import Column from './Column';
+import {getCardsForColumn, createActionAddCard} from '../../redux/cardsRedux.js';
 
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId); //aby funkcja zadziałała, musimy jej użyć w mapStateToProps
+//export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId); //aby funkcja zadziałała, musimy jej użyć w mapStateToProps
 
 const mapStateToProps = (state, props) => ({
 	cards: getCardsForColumn(state, props.id),
 });
 
-export default connect(mapStateToProps)(Column);
+const mapDispatchToProps = (dispatch, props) => ({  //funkcja mapDispatchToProps to funkcja, która nadaje komponentowi propsy, w których znajdą się funkcje wysyłające akcje do magazynu
+	addCard: title => dispatch(createActionAddCard({
+		columnId: props.id,
+		title,
+	})),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Column);
