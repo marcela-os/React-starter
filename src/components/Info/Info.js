a25eea73b7041d46fcd5dd17da2d4eae201daea4
Marcelina <m.oscislowska@gmail.com>
2020-07-28 20:50:52 +0200

    Add AnimatedSwitch

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero.js';
 import {InfoData} from '../../data/dataStore';
+import styles from './Info.scss';
 
 const Info = () => (
 	<Container>
-		<section>
+		<section className={styles.component}>
 			<Hero titleText={InfoData.title} image={InfoData.image} />
 			<div>
 				{InfoData.description}
